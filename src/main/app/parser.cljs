1cd1bfb18045b3ad1540f7a79a5fec532f8dd4a1
Cintra Guilherme <cinguilherme@gmail.com>
2022-01-23 20:49:34 -0300

    parser wip.. not working yet

--- src/main/app/parser.cljs
+++ src/main/app/parser.cljs
@@ -3,7 +3,41 @@
             [com.wsscode.pathom.connect :as pc]
             [taoensso.timbre :as log]))
 
-(def resovers [])
+(def people-table
+  {1 {:person/id 1 :person/name "Sally" :person/age 32}
+   2 {:person/id 2 :person/name "Joe" :person/age 22}
+   3 {:person/id 3 :person/name "Fred" :person/age 11}
+   4 {:person/id 4 :person/name "Bobby" :person/age 55}})
+
+(def list-table
+  {:friends {:list/id     :friends
+             :list/label  "Friends"
+             :list/people [1 2]}
+   :enemies {:list/id     :enemies
+             :list/label  "Enemies"
+             :list/people [4 3]}})
+
+(pc/defresolver person-resolver [env {:person/keys [id]}]
+  {::pc/input  #{:person/id}
+   ::pc/output [:person/name :person/age]}
+  (get people-table id))
+
+(pc/defresolver list-resolver [env {:list/keys [id]}]
+  {::pc/input  #{:list/id}
+   ::pc/output [:list/label {:list/people [:person/id]}]}
+  (when-let [list (get list-table id)]
+    (assoc list
+      :list/people (mapv (fn [id] {:person/id}) (:list/people list)))))
+
+(pc/defresolver friends-resolver [env input]
+  {::pc/output [{:friends [:list/id]}]}
+  {:friends {:list/id :friends}})
+
+(pc/defresolver enemies-resolver [env input]
+  {::pc/output [{:enemies [:list/id]}]}
+  {:enemies {:list/id :enemies}})
+
+(def resolvers [person-resolver list-resolver friends-resolver enemies-resolver])
 
 (def pathom-parser
   (p/parser {::p/env     {::p/reader                 [p/map-reader
