8982a9f1f51f165e244d666ba21c490fed154084
Cintra Guilherme <cinguilherme@gmail.com>
2022-01-23 13:57:31 -0300

    ident and mutations by namespace on atom state

--- src/main/app/mutations.cljs
+++ src/main/app/mutations.cljs
@@ -1,16 +1,13 @@
 (ns app.mutations
   (:require
-    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+    [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
 (defmutation delete-person
   "Mutation: delete person with `name` from with `list-name`"
-  [{:keys [list-name name]}]
+
+  [{list-id :list/id person-id :person/id}]
   (action [{:keys [state]}]
-          (let [path (if (= "Friends" list-name)
-                       [:friends :list/people]
-                       [:enemies :list/people])
-                old-list (get-in @state path)
-                new-list (vec (filter #(not= (:person/name %)) old-list))]
-            (swap! state assoc-in path new-list))))
+          (swap! state merge/remove-ident* [:person/id person-id] [:list/id list-id :list/people])))
 
 
--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,22 +3,25 @@
             [com.fulcrologic.fulcro.dom :as dom :refer [div p li h4 h5 ul]]
             [app.mutations :as api]))
 
-(defsc Person [this {:person/keys [name age] :as props} {:keys [onDelete]}]
+(defsc Person
+  [this {:person/keys [id name age] :as props} {:keys [onDelete]}]
+
   {:query         [:person/id :person/name :person/age]
 
    :ident         (fn [] [:person/id (:person/id props)])
 
    :initial-state (fn [{:keys [id name age] :as params}]
                     {:person/id id :person/name name :person/age age})}
+
   (li
     (h5 (str "name: " name "(age: " age ")"))
-    (dom/button {:onClick #(onDelete name)} "X")))
+    (dom/button {:onClick #(onDelete id)} "X")))
 
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
 
-(defsc PersonList [this {:keys [list/id list/label list/people] :as props}]
+(defsc PersonList [this {:list/keys [id label people] :as props}]
   {:query         [:list/id :list/label {:list/people (comp/get-query Person)}]
 
    :ident         (fn [] [:list/id (:list/id props)])
@@ -27,8 +30,10 @@
                     {:list/id     id
                      :list/label  label
                      :list/people (if (= label "Friends")
-                                    [(comp/get-initial-state Person {:id 1 :name "Sally" :age 21})]
-                                    [(comp/get-initial-state Person {:id 2 :name "Fred" :age 33})])})}
+                                    [(comp/get-initial-state Person {:id 1 :name "Sally" :age 21})
+                                     (comp/get-initial-state Person {:id 3 :name "Ford" :age 61})]
+                                    [(comp/get-initial-state Person {:id 2 :name "Fred" :age 33})
+                                     (comp/get-initial-state Person {:id 4 :name "Ferdinand" :age 37})])})}
   (let [delete-fn (fn [person-id]
                     (comp/transact!
                       this
@@ -38,14 +43,17 @@
       (ul
         (map (fn [p] (ui-person (comp/computed p {:onDelete delete-fn}))) people)))))
 
+
 (def ui-person-list (comp/factory PersonList))
+
 
 (defsc Root [this {:keys [friends enemies]}]
   {:query         [{:friends (comp/get-query PersonList)}
                    {:enemies (comp/get-query PersonList)}]
+
    :initial-state (fn [params]
-                    {:friends (comp/get-initial-state PersonList {:id 1 :label "Friends"})
-                     :enemies (comp/get-initial-state PersonList {:id 2 :label "Enemies"})})}
+                    {:friends (comp/get-initial-state PersonList {:id :friends :label "Friends"})
+                     :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})})}
 
   (dom/div
     (ui-person-list friends)
