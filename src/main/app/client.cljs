c6c0836f4a6b43631275b68a84b3a03ce8f9a036
Cintra Guilherme <cinguilherme@gmail.com>
2022-01-23 21:01:42 -0300

    wip, code suposed to use data from parser namespace.. but it is not working.. don't know why yet

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -3,7 +3,8 @@
    [app.application :refer [app]]
    [com.fulcrologic.fulcro.application :as app]
    [com.fulcrologic.fulcro.components :as comp]
-   [app.ui :refer [Root]]))
+   [com.fulcrologic.fulcro.data-fetch :as df]
+   [app.ui :as ui :refer [Root]]))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
@@ -16,6 +17,9 @@
   []
   ;; re-mounting will cause forced UI refresh, update internals, etc.
   (app/mount! app Root "app")
+  (df/load! app :friends ui/PersonList)
+  (df/load! app :enemies ui/PersonList)
+  (js/console.log "LOADED!")
   ;; As of Fulcro 3.3.0, this addition will help with stale queries when using dynamic routing:
   (comp/refresh-dynamic-queries! app)
   (js/console.log "Hot reload"))
--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -6,12 +6,9 @@
 (defsc Person
   [this {:person/keys [id name age] :as props} {:keys [onDelete]}]
 
-  {:query         [:person/id :person/name :person/age]
+  {:query [:person/id :person/name :person/age]
 
-   :ident         (fn [] [:person/id (:person/id props)])
-
-   :initial-state (fn [{:keys [id name age] :as params}]
-                    {:person/id id :person/name name :person/age age})}
+   :ident (fn [] [:person/id (:person/id props)])}
 
   (li
     (h5 (str "name: " name "(age: " age ")"))
@@ -22,18 +19,10 @@
 
 
 (defsc PersonList [this {:list/keys [id label people] :as props}]
-  {:query         [:list/id :list/label {:list/people (comp/get-query Person)}]
+  {:query [:list/id :list/label {:list/people (comp/get-query Person)}]
 
-   :ident         (fn [] [:list/id (:list/id props)])
+   :ident (fn [] [:list/id (:list/id props)])}
 
-   :initial-state (fn [{:keys [id label]}]
-                    {:list/id     id
-                     :list/label  label
-                     :list/people (if (= label "Friends")
-                                    [(comp/get-initial-state Person {:id 1 :name "Sally" :age 21})
-                                     (comp/get-initial-state Person {:id 3 :name "Ford" :age 61})]
-                                    [(comp/get-initial-state Person {:id 2 :name "Fred" :age 33})
-                                     (comp/get-initial-state Person {:id 4 :name "Ferdinand" :age 37})])})}
   (let [delete-fn (fn [person-id]
                     (comp/transact!
                       this
@@ -48,12 +37,9 @@
 
 
 (defsc Root [this {:keys [friends enemies]}]
-  {:query         [{:friends (comp/get-query PersonList)}
-                   {:enemies (comp/get-query PersonList)}]
-
-   :initial-state (fn [params]
-                    {:friends (comp/get-initial-state PersonList {:id :friends :label "Friends"})
-                     :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})})}
+  {:query [{:friends (comp/get-query PersonList)}
+           {:enemies (comp/get-query PersonList)}]
+   :initial-state {}}
 
   (dom/div
     (ui-person-list friends)
